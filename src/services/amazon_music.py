844007996a1b6ed32d1e333b48578315388298a6
bruno marques <brunomdsv5@gmail.com>
2025-02-21 10:53:54 -0300

    updated

--- src/services/amazon_music.py
+++ src/services/amazon_music.py
@@ -21,7 +21,7 @@ def config_webdriver(url):
         return None
 
 def login_amazon(driver):
-    sign_in_button = pyautogui.moveTo(1815, 228, 2)
+    sign_in_button = driver.find_element(By.XPATH, '//*[@id="signInButton"]//a').click()
     pyautogui.click()
     time.sleep(2)
     email_input = driver.find_element(By.XPATH, '//*[@id="ap_email"]').send_keys('testespootify1@gmail.com')
@@ -32,18 +32,17 @@ def login_amazon(driver):
 
 def find_playlist(driver):
     time.sleep(2)
-    pyautogui.moveTo(743, 152, 2)
-    pyautogui.moveTo(743, 220, 2)
+    pyautogui.moveTo(743, 152, 1)
+    pyautogui.moveTo(743, 220, 1)
     pyautogui.click()
-    pyautogui.moveTo(116, 317, 2)
+    pyautogui.moveTo(116, 317, 1)
     pyautogui.click()
     
 
 if __name__ == "__main__":
     driver = config_webdriver('https://music.amazon.com.br/')
-    pyautogui.moveTo(1870, 151, 2)
+    pyautogui.moveTo(1870, 151, 1)
     pyautogui.click()
-    wait = WebDriverWait(driver, 10)
     login_amazon(driver)
     find_playlist(driver)
     
--- src/services/spotify.py
+++ src/services/spotify.py
@@ -1,4 +1,4 @@
-from seleniumwire import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,40 +7,34 @@ from selenium.webdriver.support import expected_conditions as EC
 import pyautogui
 import time
 
-PROXY_HOST = "brd.superproxy.io"
-PROXY_PORT = "33335"
-PROXY_USER = "brd-customer-hl_7a3a36a2-zone-residential_proxy1"
-PROXY_PASS = "8r39pf270ihp"
-
-PROXY = f"{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}"
-
 def config_webdriver(url):
     service = Service() 
     options = webdriver.ChromeOptions()
-    
     options.add_experimental_option('detach', True)
     options.add_argument('--start-maximized')
-    options.add_argument('--proxy-server=%s' % PROXY)
 
     try:
         driver = webdriver.Chrome(service=service, options=options)
         driver.get(url)
         return driver
     except Exception as e:
-        print(f'Erro ao iniciar o webdriver com o proxy especificado')
+        print(f'Erro ao iniciar o webdriver')
         return None
 
 def login_spotify(driver):
-    username_input=driver.find_element(By.XPATH, '//*[@id="login-username"]').send_keys('testespootify1@gmail.com')
-    password_input=driver.find_element(By.XPATH, '//*[@id="login-password"]').send_keys('spootify2025')
-    login_submit=driver.find_element(By.XPATH, '//*[@id="login-button"]/span[1]').click()
-    time.sleep(2)
-    web_player_click=driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/button[2]/span[2]').click()
+    try:
+        driver.find_element(By.ID, 'login-username').send_keys('testespootify1@gmail.com') #login input
+        driver.find_element(By.ID, 'login-password').send_keys('spootify2025') #password input
+        driver.find_element(By.ID, 'login-button').click() #botao clicavel de login
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/button[2]').click() #botao clicavel do webplayer
+        
+    except Exception as e:
+        print('Erro ao tentar fazer login no spotify')
 
 def find_playlist(driver):
- 
-    search_input=driver.find_element(By.XPATH, '//*[@id="global-nav-bar"]/div[2]/div/div[1]/span/div/form/div[2]/input').send_keys('.')
-
+    driver.find_element(By.XPATH, '//*[@id="global-nav-bar"]/div[2]/div/div[1]/span/div/form/div[2]/input').send_keys('.')
+    
     time.sleep(2)
     pyautogui.moveTo(49, 293, 1)
     pyautogui.click()
@@ -51,8 +45,8 @@ def find_playlist(driver):
     pyautogui.moveTo(518, 595, 1)
     pyautogui.click()
 
-
 if __name__ == "__main__":
+
     driver = config_webdriver('https://accounts.spotify.com/en/login')
     login_spotify(driver)
     time.sleep(2)
