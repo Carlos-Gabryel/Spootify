3e6c36897d75f78ed078b861e83b3aa29ccabbd4
bruno marques <brunomdsv5@gmail.com>
2025-03-14 17:26:23 -0300

    atualizado script

--- src/auto/spotify.py
+++ src/auto/spotify.py
@@ -18,7 +18,6 @@ def setup_webdriver(url):
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36')
     options.add_argument('--headless')
-    options.add_experimental_option('detach', True)
     
     try:
         driver = webdriver.Chrome(service=service, options=options)
