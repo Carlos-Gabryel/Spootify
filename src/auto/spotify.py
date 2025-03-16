369e35ac40fa3aa32a0a339ac93a38ff522fd06e
bruno marques <brunomdsv5@gmail.com>
2025-03-16 15:42:59 -0300

    updated

--- src/auto/spotify.py
+++ src/auto/spotify.py
@@ -4,21 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import os
-
 
 # Configuração do WebDriver
 def setup_webdriver(url):
-    service = Service(executable_path="/usr/local/bin/chromedriver") 
+    service = Service() 
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
     options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36')
-    options.add_argument('--headless')
-    options.binary_location="/usr/bin/google-chrome"
+    options.add_argument('--headless=new')
     
     try:
         driver = webdriver.Chrome(service=service, options=options)
@@ -42,47 +38,57 @@ def login_spotify(driver):
         web_player = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='web-player-link']")))
         web_player.click() # botao clicavel do webplayer
 
+        print("Login bem-sucedido e webplayer selecionado.")
+
     except Exception as e:
         print(f'Erro ao tentar fazer login no spotify {e}')
 
 def buscar_playlist(driver):
-    barra_cookies = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'onetrust-close-btn-container')))
-    barra_cookies.click() # fecha a barra de cookies
+    try:
+        barra_cookies = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'onetrust-close-btn-container')))
+        barra_cookies.click() # fecha a barra de cookies
+
+        print("Barra de cookies fechada.")
+    except:
+        print("Barra de cookies não encontrada ou já fechada anteriormente, continuando...")
 
     botao_play = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[data-testid="play-button"]')))
     botao_play.click()
     
+    print("Playlist selecionada e iniciando primeira faixa...")
+
 if __name__ == "__main__":
     driver = setup_webdriver("https://accounts.spotify.com/en/login")
-    login_spotify(driver)
-    time.sleep(2)
-    buscar_playlist(driver)
-    
-    # determinando 48h de execução
-    tempo_execucao = 172800
-    tempo_inicio = time.time()
+    if driver:
+        login_spotify(driver)
+        time.sleep(2)
+        buscar_playlist(driver)
+        
+        # determinando 48h de execução
+        tempo_execucao = 172800
+        tempo_inicio = time.time()
 
-    musicas_tocadas = []
-    musica_atual = ""
+        musicas_tocadas = []
+        musica_atual = ""
 
-    count = 0
+        count = 0
 
-    while(time.time() - tempo_inicio) < tempo_execucao:
-        time.sleep(20)
-        try:
-            buscar_musica = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[4]/footer/div/div[1]/div/div[2]/div[1]/div').text
+        while(time.time() - tempo_inicio) < tempo_execucao:
+            time.sleep(20)
+            try:
+                buscar_musica = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[4]/footer/div/div[1]/div/div[2]/div[1]/div').text
 
-            if buscar_musica != musica_atual:
-                count+=1
-                musica_atual = buscar_musica
-                musicas_tocadas.append(musica_atual)
-                print(f"Nova faixa encontrada play número {count}")
-            
-        except Exception as e:
-            print(e)
+                if buscar_musica != musica_atual:
+                    count+=1
+                    musica_atual = buscar_musica
+                    musicas_tocadas.append(musica_atual)
+                    print(f"Nova faixa encontrada play número {count}")
+                
+            except Exception as e:
+                print(e)
 
-    driver.quit()
-    print(f"A quantidade de plays foi de {len(musicas_tocadas)}")
+        driver.quit()
+        print(f"A quantidade de plays foi de {len(musicas_tocadas)}")
     
         
     
