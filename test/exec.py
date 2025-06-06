87096c93b8f5fbab88b9b0a91b9c962284ce483a
Thiago <thsethub@gmail.com>
2025-06-06 18:57:17 -0300

    Atualização dos novos Ip's do websahre, remoção da autenticação do config.json, implementação do undetected webdriver, remoção do selenium-wire, correção na conexão com o proxy, implementada funções com ActionChains e verificação de propaganda no meio da contagem.

--- test/exec.py
+++ test/exec.py
@@ -2,14 +2,38 @@ import json
 import time
 import random
 import multiprocessing
+import traceback
+
+import undetected_chromedriver as uc
 from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from fake_useragent import UserAgent
 
-def setup_webdriver(proxy_host, proxy_port, proxy_user, proxy_pass):
-    options = Options()
+ua = UserAgent()
+
+def comportamento_escrever(element, text, min_delay=0.05, max_delay=0.2):
+    for char in text:
+        element.send_keys(char)
+        time.sleep(random.uniform(min_delay, max_delay))
+
+
+def comportamento_clicar(
+    driver, element, min_delay_before_click=0.5, max_delay_before_click=1.5
+):
+    action = ActionChains(driver)
+    action.move_to_element(element).perform() 
+    time.sleep(
+        random.uniform(min_delay_before_click, max_delay_before_click)
+    ) 
+    element.click()
+
+
+
+def setup_webdriver(proxy_host, proxy_port):
+    options = uc.ChromeOptions()
     # Flags para minimizar detecção de bot
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-infobars")
@@ -18,160 +42,220 @@ def setup_webdriver(proxy_host, proxy_port, proxy_user, proxy_pass):
     options.add_argument("--enable-unsafe-swiftshader")
     options.add_argument("--profile-directory=Default")
     options.add_argument("--disable-plugins-discovery")
-    options.add_argument("--start-maximized")
+    # options.add_argument("--start-maximized")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--lang=pt-BR")
-    # options.add_argument("--headless")  # Descomente para rodar sem interface gráfica
-
-    # User-Agent customizado (opcional)
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-
-    # Remove a flag de automação
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-
-    # Desabilita WebRTC para evitar vazamento de IP real
     options.add_argument("--disable-webrtc")
+    options.add_argument("--lang=pt-BR")
+    options.add_argument(f"user-agent={ua.random}")
 
-    seleniumwire_options = {
-        'proxy': {
-            'http': f'socks5://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}',
-            'https': f'socks5://{proxy_user}:{proxy_pass}@{proxy_host}:{proxy_port}',
-            'no_proxy': 'localhost,127.0.0.1'
-        }
-    }
+    options.add_argument(f"--proxy-server=http://{proxy_host}:{proxy_port}")
 
-    driver = webdriver.Chrome(options=options, seleniumwire_options=seleniumwire_options)
-
-    # Executa JS para remover propriedades de automação
-    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-        "source": """
-        Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
-        window.navigator.chrome = { runtime: {} };
-        Object.defineProperty(navigator, 'languages', {get: () => ['pt-BR', 'pt']});
-        Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
-        """
-    })
+    driver = uc.Chrome(
+        options=options,
+        headless=False,
+        # version_main=137
+    )
 
     driver.get("https://accounts.spotify.com/pt-BR")
     return driver
 
+
 def login_spotify(driver, conta):
-    # time.sleep(30000)
     email, senha = conta
     print(f"Fazendo login na conta: {email}")
-    
-    input_email = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'login-username')))
+
+    input_email = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "login-username"))
+    )
 
     if input_email:
-        input_email.send_keys(email)
-        time.sleep(2)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login-button'))).click()
+        comportamento_escrever(input_email, email) 
+        time.sleep(random.uniform(1.0, 3.0)) 
+
+        login_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "login-button"))
+        )
+        comportamento_clicar(driver, login_button)  
 
     entrar_com_senha = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.XPATH, "//button[@data-encore-id='buttonTertiary']"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//button[@data-encore-id='buttonTertiary']")
+        )
     )
 
     if entrar_com_senha:
-        entrar_com_senha.click()
-        time.sleep(2)
+        comportamento_clicar(driver, entrar_com_senha)  
+        time.sleep(random.uniform(1.5, 3.5)) 
 
-    input_senha = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'login-password')))
+    input_senha = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.ID, "login-password"))
+    )
 
     if input_senha:
-        input_senha.send_keys(senha)
-        time.sleep(2)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login-button'))).click()
+        comportamento_escrever(input_senha, senha)  
+        time.sleep(random.uniform(1.0, 3.0)) 
 
+        final_login_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "login-button"))
+        )
+        comportamento_clicar(
+            driver, final_login_button
+        )  
 
-    WebDriverWait(driver, 25).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='web-player-link']"))).click()
+    WebDriverWait(driver, 25).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "button[data-testid='web-player-link']")
+        )
+    ).click()
+
 
 playlists = [
-            'Léo Foguete 2025  🚀 As Melhores | Obrigado Deus | Última Noite | Cópia Proibida | Quem de Nós Dois', 
-            'Léo Foguete 🚀 2025 - Dona - O Coração Que Você Quer Entrar Tem Dona - Cópia Proibida - É Hit'
-        ]
+    "Léo Foguete 2025 🚀 As Melhores | Obrigado Deus | Última Noite | Cópia Proibida | Quem de Nós Dois",
+    "Léo Foguete 🚀 2025 - Dona - O Coração Que Você Quer Entrar Tem Dona - Cópia Proibida - É Hit",
+]
 
-def tocar_playlist(driver, nome_playlist=random.choice(playlists)): 
+
+def tocar_playlist(driver, nome_playlist=random.choice(playlists)):
+    print(f"Login realizado com sucesso. Selecionando a playlist: {nome_playlist}")
+
     try:
-        # Clica na playlist com base no nome
         playlist = WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, f'//div[@aria-label="Sua Biblioteca"]//div[@role="row" and .//button[contains(@aria-label, "Tocar {nome_playlist}")]]'
-                )))
-        playlist.click()
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    f'//div[@aria-label="Sua Biblioteca"]//div[@role="row" and .//button[contains(@aria-label, "Tocar {nome_playlist}")]]',
+                )
+            )
+        )
+        comportamento_clicar(driver, playlist)  
         print(f"Playlist encontrada: {nome_playlist}")
     except Exception as e:
         print(f"Não foi possível selecionar a playlist {nome_playlist} - {e}")
+        time.sleep(40000)
 
-    time.sleep(random.uniform(8, 12))
+    time.sleep(random.uniform(8, 12)) 
     try:
         botao_play = WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable((
-                By.XPATH, f'//div[@aria-label="Sua Biblioteca"]//div[@role="group" and @data-encore-id="listRow"]//button[@data-encore-id="buttonTertiary" and @aria-label="Tocar {nome_playlist}"]'
-                )))
-        botao_play.click()
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    f'//div[@aria-label="Sua Biblioteca"]//div[@role="group" and @data-encore-id="listRow"]//button[@data-encore-id="buttonTertiary" and @aria-label="Tocar {nome_playlist}"]',
+                )
+            )
+        )
+        comportamento_clicar(driver, botao_play) 
         print(f"Playlist '{nome_playlist}' tocada com sucesso.")
     except Exception as e:
         print(f"Erro ao tocar a playlist '{nome_playlist}': {e}")
-        driver.quit()
+        driver.quit() 
+
 
 def logout_spotify(driver, email=None):
     if email:
         print(f"Fazendo logout da conta: {email}")
 
     driver.get("https://accounts.spotify.com/pt-BR/logout")
-    time.sleep(2)
+    time.sleep(random.uniform(2.0, 4.0)) 
+
+def fechar_cookies(driver):
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'onetrust-close-btn-container'))).click()
+        print("Barra de cookies fechada")
+
+    except:
+        print("Barra de cookies não encontrada ou já fechada anteriormente, continuando...")
+
 
 def worker(proxy_info):
     proxy_host = proxy_info["host"]
     proxy_port = proxy_info["port"]
-    proxy_user = proxy_info["username"]
-    proxy_pass = proxy_info["password"]
     contas = proxy_info["contas"]
     index = 0
+    propaganda = False
 
     while True:
         conta = contas[index]
-        print(f"[Proxy {proxy_host}:{proxy_port}] Ciclo conta: {conta[0]}")
-        driver = setup_webdriver(proxy_host, proxy_port, proxy_user, proxy_pass)
-        musica_atual = ""
-        contador_plays = 0
+        email_atual = conta[0]
+        print(f"[Proxy {proxy_host}:{proxy_port}] Ciclo conta: {email_atual}")
+        driver = None
         try:
+            driver = setup_webdriver(proxy_host, proxy_port)
+            musica_atual = ""
+            contador_plays = 0
+
             login_spotify(driver, conta)
             tocar_playlist(driver)
+
             wait_time = random.uniform(3600, 4000)
-            print(f"[Proxy {proxy_host}:{proxy_port}] Aguardando {wait_time/60:.2f} minutos")
+            print(
+                f"[Proxy {proxy_host}:{proxy_port}] Aguardando {wait_time/60:.2f} minutos"
+            )
 
             start_time = time.time()
             while time.time() - start_time < wait_time:
                 try:
                     try:
-                        buscar_musica = driver.find_element(By.CSS_SELECTOR, '[data-testid="now-playing-widget"]').text
-                        buscar_musica = buscar_musica.replace("Tocando agora", "").strip()
-                    except:
-                        print("Musica nao encontrada, anuncio em tela.")
+                        buscar_musica = driver.find_element(
+                            By.CSS_SELECTOR, '[data-testid="now-playing-widget"]'
+                        ).text
+                        buscar_musica = buscar_musica.replace(
+                            "Tocando agora", ""
+                        ).strip()
+                        time.sleep(2)
+                        propaganda = False
+                    except Exception:
+                        print("Musica nao encontrada. Relizando verificações.")
+                        
+                        try:
+                            buscar_propaganda = driver.find_element(
+                            By.XPATH, '//div[@data-testid="now-playing-widget" and @aria-label="Propaganda"]'
+                        )
+                            if buscar_propaganda:
+                                propaganda = True
+                                time.sleep(5)
+                                continue
 
-                    if buscar_musica != musica_atual:
+                        except Exception:
+                            fechar_cookies(driver)
+
+                    if (buscar_musica != musica_atual) and propaganda is False:
                         contador_plays += 1
                         musica_atual = buscar_musica
-                        print(f"[Proxy {proxy_host}:{proxy_port}] Nova faixa encontrada play: {contador_plays}")
+                        print(
+                            f"[Proxy {proxy_host}:{proxy_port}] Nova faixa encontrada play: {contador_plays}"
+                        )
 
                 except Exception as e:
-                    print(f'[Proxy {proxy_host}:{proxy_port}] Erro ao buscar música: {e}')
-                time.sleep(10)  # Ajuste o intervalo conforme necessário
+                    print(
+                        f"[Proxy {proxy_host}:{proxy_port}] Erro ao buscar música: {e}"
+                    )
+                time.sleep(random.uniform(10, 25))  
 
         except Exception as e:
-            print(f"[Proxy {proxy_host}:{proxy_port}] Erro: {e}")
+            print(
+                f"[Proxy {proxy_host}:{proxy_port}] Erro crítico no ciclo para conta {email_atual}: {e}"
+            )
+            print(traceback.format_exc())
         finally:
-            logout_spotify(driver)
-            driver.quit()
-            print(f"[Proxy {proxy_host}:{proxy_port}] Driver encerrado. Alternando conta.")
-            index = (index + 1) % len(contas)
-            time.sleep(5)
+            if driver:
+                logout_spotify(driver, email_atual)
+                driver.quit()
+                print(
+                    f"[Proxy {proxy_host}:{proxy_port}] Driver encerrado. Alternando conta."
+                )
+            else:
+                print(
+                    f"[Proxy {proxy_host}:{proxy_port}] Driver não foi inicializado para conta {email_atual}. Pulando para a próxima."
+                )
 
-if __name__ == '__main__':
-    with open('config.json') as f:
+            index = (index + 1) % len(contas)
+            time.sleep(random.uniform(5, 15))  
+
+
+if __name__ == "__main__":
+    with open("config.json") as f:
         config = json.load(f)
 
     processos = []
